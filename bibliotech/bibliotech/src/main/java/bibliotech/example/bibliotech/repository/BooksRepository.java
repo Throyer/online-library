599037a04b7aa2e7d53003fdf3c5479206e6caf1
Lucas De Oliveira C. <117128804+Domlcforce123@users.noreply.github.com>
2024-07-31 21:44:19 -0300

    Add files via upload
    Commit do começo da bibliotech

---
+++ bibliotech/bibliotech/src/main/java/bibliotech/example/bibliotech/repository/BooksRepository.java
@@ -0,0 +1,12 @@
+package bibliotech.example.bibliotech.repository;
+
+import bibliotech.example.bibliotech.models.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BooksRepository extends JpaRepository <Book, Long> {
+    List<Book> findByAutor(String autor);
+    List<Book> findByTitulo(String titulo);
+    List<Book> findByGenero(String genero);
+}
